3ab42ff5891d448bd59043a0e583cb79095e2c99
Nirweint <nirweint_1@mail.ru>
2022-03-21 18:11:26 +0300

    client works

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from "@mui/material/Button";
 
 const Index = () => {
   return (
     <div>
       Main page
+      <Button>Hello</Button>
     </div>
   );
 };
